454db212e0dc9d27ab5ac83a0e03ee6d420bda94
Snow <bis23-smsumba@mubas.ac.mw>
2025-12-18 17:25:01 +0200

    prepare project for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+output: {
+  filename: "main.js",
+  path: path.resolve(__dirname, "dist"),
+  clean: true,
+  publicPath: "./",
+}
+,
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/index.html"],
