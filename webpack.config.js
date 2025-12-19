a85e29fe0f6df4d2704d0457977d4ea5795f2834
Snow <bis23-smsumba@mubas.ac.mw>
2025-12-19 08:50:23 +0200

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,14 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production", 
   entry: "./src/index.js",
-output: {
-  filename: "main.js",
-  path: path.resolve(__dirname, "dist"),
-  clean: true,
-  publicPath: "./",
-}
-,
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/index.html"],
-      static: "./dist",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/YOUR_REPO_NAME/", 
   },
   plugins: [
     new HtmlWebpackPlugin({
